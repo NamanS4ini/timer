d55c9b8ae3017946de188bb4d183dc8180abc858
Naman Saini <namansaini464@gmail.com>
2025-06-29 17:12:51 +0530

    Initial and final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Timer from "@/components/Timer";
+import Timer from "../components/Timer";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
@@ -6,3 +6,7 @@ export default function Home() {
     </div>
   );
 }
+export const metadata = {
+  title: "Timer App",
+  description: "A simple timer application built with Next.js",
+};
